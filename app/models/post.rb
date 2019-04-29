608bc30feb9ea639b15ad77c414021129cdd4b7a
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-04-28 18:46:20 -0700

    add dynamic translation storage system

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,19 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5 }
+  after_save :update_i18n
 
-  def pretty_created_at
+  def translated(attribute)
+    I18n.t(attribute.to_s, scope: id.to_s, default: self[attribute])
+  end
+
+  def update_i18n
+    I18n.backend.store_translations(I18n.default_locale,
+      {
+        id => {
+          title: title,
+          text: text
+        }
+      }
+    )
   end
 end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,6 +1,39 @@
 # Enable additional languages
 I18n.available_locales = [:en, :fr, :it, :es].sort
 
+# Load JSON as well as .rb and .yml
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{json}')]
+
+# Define a custom I18n backend that will write new translations received at
+# runtime via store_translations to a standard translation file for later
+# loading
+class CrowdinBackend < I18n::Backend::Simple
+  def store_translations(locale, data, options = I18n::EMPTY_HASH)
+    super(locale, data, options)
+    write_translations(locale, data) if initialized?
+  end
+
+  private
+
+  def write_translations(locale, new_data)
+    new_data = new_data.deep_stringify_keys
+    existing_data = locale_data(locale)
+    existing_data[locale.to_s].deep_merge!(new_data)
+    File.write(locale_file(locale), JSON.pretty_generate(existing_data))
+  end
+
+  def locale_data(locale)
+    JSON.parse(File.read(locale_file(locale)))
+  rescue StandardError
+    { locale.to_s => {}}
+  end
+
+  def locale_file(locale)
+    Rails.root.join("config/locales/crowdin.#{locale}.json")
+  end
+end
+I18n.backend = CrowdinBackend.new
+
 # Default to English when translation not found
 old_handler = I18n.exception_handler
 I18n.exception_handler = lambda do |exception, locale, key, options|
